a09d9562e4deb90888091fd80ee00b35229ab97a
snailcreature <samuel.drage@myport.ac.uk>
2022-05-23 00:40:09 +0100

    Added Round rendering

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -2,15 +2,25 @@ import '../css/index.css';
 
 const { Tournament } = require('./rounds');
 
+const rounds = document.querySelector('#rounds');
+
 const entrants = [
   "Sam",
   "Hyrum",
   "Danielle",
   "Dan",
-  "Dyllan",
+  "Dylan",
   "Tom",
 ];
 
 let tourn = new Tournament("tourney", entrants);
 
-console.log(tourn.getRounds());
+const roundList = tourn.getRounds();
+for (let i = 1; i <= tourn.roundCount; i++) {
+  rounds.innerHTML += `<h2>Round ${i}</h2>`;
+  rounds.innerHTML += `<ol>`;
+  roundList[i].getMatches().forEach(match => {
+    rounds.innerHTML += `<li>${match.entA} vs ${match.entB}</li>`;
+  });
+  rounds.innerHTML += `</ol>`
+}
