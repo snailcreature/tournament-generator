9dcc65bc1131f72e7ae33f55a691412605eaef30
snailcreature <samuel.drage@myport.ac.uk>
2022-05-23 15:44:54 +0100

    Added round winner select

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -17,30 +17,82 @@ const edit = document.querySelector('#edit');
 
 let tournament = undefined;
 
-create.addEventListener('click', () => {
+function buildLayout() {
   const entrants = entrantList.value.split('\n');
   if (entrants.length >= 0 && entrants.every(ent => ent !== '')) {
     if (tournament === undefined || tournament.entrants.length !== entrants.length) {
       tournament = new Tournament("tournament", entrants);
+      const roundList = tournament.getRounds();
+      rounds.innerHTML = '<h2>Tournament</h2>';
+      for (let i = 1; i <= tournament.roundCount; i++) {
+        rounds.innerHTML += `<h3>Round ${i}</h3>`;
+        rounds.innerHTML += `<ol>`;
+        roundList[i].getMatches().forEach(match => {
+          rounds.innerHTML += `<li>${match.entA} vs ${match.entB}</li>`;
+
+          const winSelect = document.createElement('select');
+          const blankOpt = document.createElement('option');
+          blankOpt.value = 'null';
+          const entAOpt = document.createElement('option');
+          entAOpt.value = match.entA;
+          entAOpt.textContent = match.entA;
+          const entBOpt = document.createElement('option');
+          entBOpt.value = match.entB;
+          entBOpt.textContent = match.entB;
+          winSelect.appendChild(blankOpt);
+          winSelect.appendChild(entAOpt);
+          winSelect.appendChild(entBOpt);
+          winSelect.onclick = function () {
+            console.log("HELLO")
+          }
+          console.log({winSelect})
+          rounds.appendChild(winSelect);
+        });
+        rounds.innerHTML += `</ol>`;
+      }
     } else if (!entrants.every(ent => tournament.entrants.includes(ent))) {
       tournament = new Tournament("tournament", entrants);
+      const roundList = tournament.getRounds();
+      rounds.innerHTML = '<h2>Tournament</h2>';
+      for (let i = 1; i <= tournament.roundCount; i++) {
+        rounds.innerHTML += `<h3>Round ${i}</h3>`;
+        rounds.innerHTML += `<ol>`;
+        roundList[i].getMatches().forEach(match => {
+          rounds.innerHTML += `<li>${match.entA} vs ${match.entB}</li>`;
+
+          const winSelect = document.createElement('select');
+          const blankOpt = document.createElement('option');
+          blankOpt.value = 'null';
+          const entAOpt = document.createElement('option');
+          entAOpt.value = match.entA;
+          const entBOpt = document.createElement('option');
+          entBOpt.value = match.entB;
+          entBOpt.textContent = match.entB;
+          winSelect.appendChild(blankOpt);
+          winSelect.appendChild(entAOpt);
+          winSelect.appendChild(entBOpt);
+          rounds.appendChild(winSelect);
+        });
+        rounds.innerHTML += `</ol>`;
+      }
     }
-    const roundList = tournament.getRounds();
-    rounds.innerHTML = '<h2>Tournament</h2>';
-    for (let i = 1; i <= tournament.roundCount; i++) {
-      rounds.innerHTML += `<h3>Round ${i}</h3>`;
-      rounds.innerHTML += `<ol>`;
-      roundList[i].getMatches().forEach(match => {
-        rounds.innerHTML += `<li>${match.entA} vs ${match.entB}</li>`;
-      });
-      rounds.innerHTML += `</ol>`
-    }
+    
     tournamentSection.hidden = false;
     configSection.hidden = true;
   }
-});
+}
+
+create.addEventListener('click', buildLayout);
 
 edit.addEventListener('click', () => {
   configSection.hidden = false;
   tournamentSection.hidden = true;
+});
+
+document.addEventListener('input', (e) => {
+  if (e.target.localName !== 'select' || e.target.tagName !== 'SELECT') return;
+
+  if (!e.target[0].hidden) e.target[0].hidden = true;
+
+  console.log(e.target.value);
 });
